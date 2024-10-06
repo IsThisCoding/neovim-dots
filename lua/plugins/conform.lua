9310952351a2c587c7d76a95a7f425c90fceead0
IsThisCoding <166351231+IsThisCoding@users.noreply.github.com>
2024-10-06 16:19:02 -0400

    updated config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,30 +1,31 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
 
-	config = function()
-		local conform = require("conform")
+  config = function()
+    local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				svelte = { { "prettierd", "prettier" } },
-				javascript = { { "prettierd", "prettier" } },
-				json = { { "prettierd", "prettier" } },
-				typescript = { { "prettierd", "prettier" } },
-				java = { "google-java-format" },
-				html = { "htmlbeautifier" },
-				css = { { "prettierd", "prettier" } },
-				bash = { "beautysh" },
-			},
-		})
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        svelte = { { "prettierd", "prettier" } },
+        javascript = { { "prettierd", "prettier" } },
+        json = { { "prettierd", "prettier" } },
+        typescript = { { "prettierd", "prettier" } },
+        java = { "google-java-format" },
+        html = { "htmlbeautifier" },
+        css = { { "prettierd", "prettier" } },
+        bash = { "beautysh" },
+        jsonc = { "prettierd", "prettier" }
+      },
+    })
 
-		vim.keymap.set({ "n", "v" }, "<leader>gf", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500,
-			})
-		end, { desc = "Format file or (v) selection" })
-	end,
+    vim.keymap.set({ "n", "v" }, "<leader>gf", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end, { desc = "Format file or (v) selection" })
+  end,
 }
--- lua/plugins/live-server.lua
+++ lua/plugins/live-server.lua
@@ -1,20 +1,17 @@
 return {
-	"ngtuonghy/live-server-nvim",
-	event = "VeryLazy",
-	build = ":LiveServerInstall",
-	config = function()
-		require("live-server-nvim").setup({
-			require("live-server-nvim").setup({
-				custom = {
-					"--port=9999",
-					"--no-css-inject",
-				},
-				serverPath = vim.fn.stdpath("data") .. "/live-server/", --default
-				open = "folder", -- folder|cwd     --default
-			}),
-		})
-		vim.keymap.set("n", "<leader>lt", function()
-			require("live-server-nvim").toggle()
-		end)
-	end,
+  "ngtuonghy/live-server-nvim",
+  event = "VeryLazy",
+  build = ":LiveServerInstall",
+  config = function()
+    require("live-server-nvim").setup {
+      custom = {
+        "--port=9999",
+        "--no-css-inject",
+      },
+      serverPath = vim.fn.stdpath("data") .. "/live-server/", --default
+      open = "folder",                                        -- folder|cwd     --default
+    }
+    vim.keymap.set("n", "<leader>lt", function() require("live-server-nvim").toggle() end, { desc = "Toggle Live Server" })
+  end
 }
+
